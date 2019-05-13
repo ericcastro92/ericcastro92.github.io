f2f9703617e82d720f5e95c12972ba57408fe610
Eric Castro <ericcastro@Erics-MBP-2.lan>
2019-05-12 20:14:13 -0700

    Site

---
+++ precache-manifest.1fb1d188015a628fb7bb00d33dd54198.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e234ccecd6b22d053bb07aae8744c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12782cf07ec93b14dfb6",
+    "url": "/static/css/main.e148e9fc.chunk.css"
+  },
+  {
+    "revision": "e56f8c9eb1eef53e91dd",
+    "url": "/static/js/2.c0d87795.chunk.js"
+  },
+  {
+    "revision": "12782cf07ec93b14dfb6",
+    "url": "/static/js/main.a74cdd05.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50c63ebc60e4f6f502dd09b59c55146b",
+    "url": "/static/media/BebasNeue-Bold.50c63ebc.ttf"
+  },
+  {
+    "revision": "2594d6375c3e55ec7e5f384803cbf51e",
+    "url": "/static/media/BebasNeue-Light.2594d637.ttf"
+  },
+  {
+    "revision": "54413459a5adf3f82686db50595cba5a",
+    "url": "/static/media/BebasNeue-Regular.54413459.ttf"
+  },
+  {
+    "revision": "a8af7ade5b15718cf172baeb90d9c585",
+    "url": "/static/media/BebasNeue-Thin.a8af7ade.ttf"
+  }
+]);
